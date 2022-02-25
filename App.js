757e5938a5bd57aaa6324936816c7a13a4afbda8
AkiraHonda <05410kira@gmail.com>
2022-02-25 14:25:11 +0900

    StackNavigatorの利用

--- App.js
+++ App.js
@@ -1,69 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
-import ListItem from './components/ListItem';
-import dummyArticles from './dummies/articles';
-import Constants from 'expo-constants';
-import axios from 'axios';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles)
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-          imageUrl={item.urlToImage}
-          title={item.title}
-          author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    borderColor: "gray",
-    borderWidth: 1,
-    flexDirection: "row",
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between",
-  },
-  text: {
-    fontSize: 16
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray"
-  }
-});
