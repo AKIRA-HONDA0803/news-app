2231daf5afd01358b1872a0208d1e1235cef402f
AkiraHonda <05410kira@gmail.com>
2022-02-26 23:38:57 +0900

    記事のクリップへの追加・削除

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, TouchableOpacity, Text } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from "../store/actions/user";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,9 +13,22 @@ const styles = StyleSheet.create({
 
 export default function ArticleScreen ({route}) {
   const {article} = route.params;
+  const dispatch = useDispatch();
   console.log(article.url);
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({clip: article}));
+        }}>
+        <Text style={{margin: 10, fontSize: 30}}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({clip: article}));
+        }}>
+        <Text style={{margin: 10, fontSize: 30}}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView
       source={{uri: article.url}}
       />
