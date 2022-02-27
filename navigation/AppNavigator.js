3d193a8b58c20a3eecb243d26bcbb7322a0db8cc
AkiraHonda <05410kira@gmail.com>
2022-02-27 21:28:47 +0900

    画面下にタブを追加

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,13 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ClipScreen from '../screens/ClipScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function AppNavigator () {
+const HomeStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <Stack.Navigator>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -20,6 +22,27 @@ export default function AppNavigator () {
           component={ArticleScreen}
         />
       </Stack.Navigator>
+  );
+};
+
+const ClipStack = () => {
+  return (
+    <Stack.Navigator>
+        <Stack.Screen
+          name="Clip"
+          component={ClipScreen}
+        />
+      </Stack.Navigator>
+  );
+};
+
+export default function AppNavigator () {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Clip" component={ClipStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
