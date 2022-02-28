b0445e219648bf5c09038cb727ccb896de99022b
AkiraHonda <05410kira@gmail.com>
2022-02-28 10:22:38 +0900

    クリップボタンにアイコン表示

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -33,6 +33,10 @@ const ClipStack = () => {
           name="Clip"
           component={ClipScreen}
         />
+        <Stack.Screen
+          name="Article"
+          component={ArticleScreen}
+        />
       </Stack.Navigator>
   );
 };
