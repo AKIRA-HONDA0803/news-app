f84e8ea208ca692ea36bfd8f612746e2a0a67fe2
AkiraHonda <05410kira@gmail.com>
2022-03-02 17:12:05 +0900

    ローディング画面の設定

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -4,6 +4,7 @@ import { WebView } from 'react-native-webview';
 import { useDispatch, useSelector } from 'react-redux';
 import { addClip, deleteClip } from "../store/actions/user";
 import ClipButton from "../components/ClipButton";
+import Loading from '../components/Loading';
 
 const styles = StyleSheet.create({
   container: {
@@ -37,7 +38,9 @@ export default function ArticleScreen ({route}) {
     <SafeAreaView style={styles.container}>
       <ClipButton onPress={toggleClip} enabled={isClipped()} />
       <WebView
-      source={{uri: article.url}}
+        source={{uri: article.url}}
+        startInLoadingState={true}
+        renderLoading={() => <Loading />}
       />
     </SafeAreaView>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,23 +3,27 @@ import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from '../components/ListItem';
 import Constants from 'expo-constants';
 import axios from 'axios';
+import Loading from '../components/Loading';
 
 const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
 
 
 export default function HomeScreen (props) {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     fetchArticles();
   }, []);
 
   const fetchArticles = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(URL);
       setArticles(response.data.articles)
     } catch (error) {
       console.error(error);
     }
+    setLoading(false);
   };
 
   return (
@@ -36,6 +40,7 @@ export default function HomeScreen (props) {
         )}
         keyExtractor={(item, index) => index.toString()}
       />
+      {loading && <Loading />}
     </SafeAreaView>
   );
 }
